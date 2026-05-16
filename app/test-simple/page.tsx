e4e6f771d3557980286420055af73c713177c6f5
CollectaProduce <corporativocollecta@gmail.com>
2026-05-16 09:56:30 -0600

    Add test page for diagnostics

---
+++ app/test-simple/page.tsx
@@ -0,0 +1,9 @@
+export default function TestSimple() {
+  return (
+    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
+      <h1>Test Page - Simple</h1>
+      <p>If you see this, the basic routing is working!</p>
+      <p>This is a test page to diagnose the 404 issue.</p>
+    </div>
+  );
+}
